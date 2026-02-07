f833ffb0b14a566d3364834abc0e321e0a1eebc9
skin-soc <82213990+skin-soc@users.noreply.github.com>
2026-02-07 03:39:45 +0100

    Update open-next.config.ts with R2 cache comments
    Added comments for R2 cache configuration in Cloudflare.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,9 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-  image: {
-    deliveryURL: "https://gusmcewan.com",  // Or your custom domain
-    variant: "public",  // Matches the variant name you created
-  },
+  // Uncomment to enable R2 cache,
+  // It should be imported as:
+  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
+  // See https://opennext.js.org/cloudflare/caching for more details
+  // incrementalCache: r2IncrementalCache,
 });
