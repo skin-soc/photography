e6fc6c942bc548aff90cbce4fa8eb89ab612081b
skin-soc <82213990+skin-soc@users.noreply.github.com>
2026-04-12 01:17:19 +0200

    Refactor image handling and update title styling
    Refactored image handling to use <picture> element for better responsiveness and added WebkitTextStroke to the title based on theme.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 'use client'
 
-import Image from 'next/image'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
-  // Effect to detect theme changes
   useEffect(() => {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     const handleChange = (e: MediaQueryListEvent) => {
       setIsDarkMode(e.matches);
     };
 
-    // Set initial theme
     setIsDarkMode(mediaQuery.matches);
     mediaQuery.addEventListener('change', handleChange);
 
-    // Cleanup listener on unmount
     return () => mediaQuery.removeEventListener('change', handleChange);
   }, []);
 
@@ -25,26 +21,31 @@ export default function Home() {
     <div className="relative min-h-screen">
       {/* Hero Image */}
       <div className="absolute inset-0">
-        <source srcSet={isDarkMode ? '/images/dark.webp' : '/images/light.webp'} type="image/webp" />
-        <Image
-          src={isDarkMode ? '/images/dark.jpg' : '/images/light.jpg'}
-          alt="Gus McEwan's architectural photograph"
-          fill
-          className="object-cover brightness-99 object-left-top md:object-center"
-          priority
-          sizes="100vw"
-          style={{ objectPosition: '21% 0' }}
-          quality={100}
-          unoptimized={true}
-        />
+        <picture>
+          <source srcSet={isDarkMode ? '/images/dark.webp' : '/images/light.webp'} type="image/webp" />
+          <img
+            src={isDarkMode ? '/images/dark.jpg' : '/images/light.jpg'}
+            alt="Gus McEwan's photograph"
+            loading="eager"
+            className="object-cover brightness-99 object-left-top md:object-center w-full h-full"
+            style={{ objectPosition: '21% 0' }}
+          />
+        </picture>
       </div>
 
       {/* Overlay Content */}
       <div className="relative min-h-screen flex flex-col items-center justify-center px-4">
         <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
-          <h1 className="text-[15vw] md:text-[12vw] lg:text-[14vw] text-black dark:text-white tracking-[0.2em]">McEWAN</h1>
+          <h1
+            className="text-[15vw] md:text-[12vw] lg:text-[14vw] text-black dark:text-white tracking-[0.2em]"
+            style={{
+              WebkitTextStroke: isDarkMode ? '1px black' : '1px white',
+            }}
+          >
+            McEWAN
+          </h1>
         </div>
       </div>
     </div>
   )
-} 
+}
