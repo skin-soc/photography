38c14ed7d9855732702942efdae238881a9b6ae6
skin-soc <82213990+skin-soc@users.noreply.github.com>
2026-04-13 00:15:42 +0200

    Add PNG favicon link to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+        <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Gus McEwan" />
